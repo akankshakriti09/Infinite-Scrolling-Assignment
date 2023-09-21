56ab871e02c71446775da0171ce7bd358b030d8b
Akanksha Kriti <akankshakriti09@gmail.com>
2023-09-21 19:37:03 +0530

    changes

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,9 +4,9 @@ export class MenuItem extends Component {
   render() {
     let {item, description, imageUrl} = this.props; //destructuring 
     return (
-      <div>
-            <div className="card" style={{width: "18rem"}}>
-              <img src="imageUrl" className="card-img-top" alt="..."/>
+      <div className="my-2">
+            <div className="card" style={{width: "12rem"}}>
+              <img src= {imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{item}</h5>
                         <p className="card-text">{description}</p>
--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -4,12 +4,12 @@ import MenuItem from "./MenuItem";
 export class RestroMenu extends Component {
   render() {
     return (
-      <div className="container my-5">
+      <div className="container my-2" >
         <h2>Restaurant Menu</h2>
-        {/*MenuItem Component added here col-md-5 */}
+        {/*MenuItem Component added here*/}
         <div className="row">
               <div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" />
+                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800" />
               </div>
               <div className="col-md-2">
                 <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
@@ -26,7 +26,7 @@ export class RestroMenu extends Component {
         </div>
         <div className="row">
               <div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" />
+                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800"/>
               </div>
               <div className="col-md-2">
                 <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
