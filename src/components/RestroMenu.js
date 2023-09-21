fba154b6fe6ef523e2e738579b0de843ff9d2ec4
Akanksha Kriti <akankshakriti09@gmail.com>
2023-09-21 22:35:02 +0530

    json data added

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -2,14 +2,50 @@ import React, { Component } from "react";
 import MenuItem from "./MenuItem";
 
 export class RestroMenu extends Component {
+  constructor(){
+    super();
+    console.log("Hello I am Constructor from RestroMenu Component");
+    this.state = {
+      dishes: this.dishes,
+      loading: false
+    }
+}
+
   render() {
     return (
       <div className="container my-2" >
         <h2>Restaurant Menu</h2>
         {/*MenuItem Component added here*/}
         <div className="row">
+                  {this?.state?.dishes?.map((element) => {
+                    return <div className="col-md-4"  key={element.id}>
+                        <MenuItem title={element.name} description={element.description} imageUrl={element.image} />
+                        <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150" />
+                </div>
+
+                  })}
+
+
+          
+              {/*<div className="col-md-2">
+                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150" />
+              </div>
+               <div className="col-md-2">
+                <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
+              </div>
               <div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800" />
+                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
+              </div>
+              <div className="col-md-2">
+                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
+              </div>
+              <div className="col-md-2">
+                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
+              </div> */}
+        </div>
+        {/* <div className="row">
+              <div className="col-md-2">
+                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150"/>
               </div>
               <div className="col-md-2">
                 <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
@@ -23,24 +59,7 @@ export class RestroMenu extends Component {
               <div className="col-md-2">
                 <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
               </div>
-        </div>
-        <div className="row">
-              <div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800"/>
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-        </div>
+        </div> */}
       </div>
     );
   }
