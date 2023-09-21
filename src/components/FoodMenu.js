8d46ab5907ac9d179e994015c3f6f7b6138b8e01
Akanksha Kriti <akankshakriti09@gmail.com>
2023-09-21 17:28:41 +0530

    Component created and added to another component

---
+++ src/components/FoodMenu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import MenuItem from './MenuItem'
+
+export class FoodMenu extends Component {
+  render() {
+    return (
+      <div>
+        This is a thidhphs
+        <FoodMenu/>
+        <FoodMenu/>
+        <FoodMenu/>
+        <FoodMenu/>
+        <FoodMenu/>
+        <FoodMenu/>
+      </div>
+    )
+  }
+}
+
+export default FoodMenu
---
+++ src/components/MenuItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MenuItem extends Component {
+  render() {
+    return (
+      <div>
+        This is a food item.
+      </div>
+    )
+  }
+}
+
+export default MenuItem
