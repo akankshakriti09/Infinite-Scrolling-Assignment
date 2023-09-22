23fb19b4fe6dfaa4d66418c452bf6b3bf9ba6cbe
Akanksha Kriti <akankshakriti09@gmail.com>
2023-09-22 06:44:40 +0530

    json api url added

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import MenuItem from "./MenuItem";
 
 export class RestroMenu extends Component {
+
   constructor(){
     super();
     console.log("Hello I am Constructor from RestroMenu Component");
     this.state = {
-      dishes: this.dishes,
-      loading: false
+      products: [],
+      loading: false,
+      page: 1
     }
 }
+
+    async componentDidMount(){
+      let url = "https://api.imgflip.com/get_memes?page=:1";
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({products: parsedData.products})
+    }
 
   render() {
     return (
@@ -17,49 +27,22 @@ export class RestroMenu extends Component {
         <h2>Restaurant Menu</h2>
         {/*MenuItem Component added here*/}
         <div className="row">
-                  {this?.state?.dishes?.map((element) => {
-                    return <div className="col-md-4"  key={element.id}>
-                        <MenuItem title={element.name} description={element.description} imageUrl={element.image} />
-                        <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150" />
-                </div>
+                  {this?.state?.products?.map((element) => {
+                           return <div className="col-md-2"  key={element.id}>
+                                 <MenuItem item={element.name} description={element.width} imageUrl={element.url}/>
+                         </div> 
+                         })                       
+                  }    
 
-                  })}
-
-
-          
-              {/*<div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150" />
-              </div>
-               <div className="col-md-2">
-                <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div> */}
+                  <div>
+                    <div className="container">
+                      <button type="button" class="btn btn dark">Previous</button>
+                      <button type="button" class="btn btn dark">Next</button>
+                    </div>
+                  </div>
+         
         </div>
-        {/* <div className="row">
-              <div className="col-md-2">
-                <MenuItem item="Burger" description="bguadouD8AEGSBOA" imageUrl="https://via.placeholder.com/150"/>
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Pizza" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-              <div className="col-md-2">
-                <MenuItem item="Tea" description="bguadouD8AEGSBOA" />
-              </div>
-        </div> */}
+
       </div>
     );
   }
